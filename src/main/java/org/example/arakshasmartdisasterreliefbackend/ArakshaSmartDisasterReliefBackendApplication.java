31e45ec5f33c56f61e3bc4c1d1b91d607854b4f5
Ravindu387 <ravindunim6121@gmail.com>
2026-06-17 13:10:58 +0530

    initial commit

---
+++ src/main/java/org/example/arakshasmartdisasterreliefbackend/ArakshaSmartDisasterReliefBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.arakshasmartdisasterreliefbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArakshaSmartDisasterReliefBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArakshaSmartDisasterReliefBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/arakshasmartdisasterreliefbackend/ArakshaSmartDisasterReliefBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.arakshasmartdisasterreliefbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArakshaSmartDisasterReliefBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
